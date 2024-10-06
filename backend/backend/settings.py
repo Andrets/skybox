96d827f9f679513cff5a36c8ea485cd3686cd14b
Andrets <dmitriysurkov2019@gmail.com>
2024-10-06 11:53:04 +0500

    fast fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -168,6 +168,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Настройки хранения медиафайлов
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
