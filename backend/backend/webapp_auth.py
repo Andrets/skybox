4a7d4b1c28aa4b128f990761768939bab599e990
Swydk <sanzhar.kasymzhomart@gmail.com>
2024-10-09 18:19:40 +0500

    fix

--- backend/backend/webapp_auth.py
+++ backend/backend/webapp_auth.py
@@ -43,8 +43,8 @@ class WebAppAuth:
             parsed_data = dict(parse_qsl(init_data, strict_parsing=True))
         except ValueError as err:
             raise AuthError(detail="invalid init data") from err
-        if "hash" not in parsed_data:
-            raise AuthError(detail=f"missing hash {parsed_data}")
+        """ if "hash" not in parsed_data:
+            raise AuthError(detail=f"missing hash {parsed_data}") """
         hash_ = parsed_data.pop("hash")
         data_check_string = "\n".join(f"{k}={v}" for k, v in sorted(parsed_data.items(), key=itemgetter(0)))
         calculated_hash = hmac.new(
